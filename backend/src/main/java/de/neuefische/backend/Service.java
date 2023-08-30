8987f803928a45cce3dc5af0122d98aab7ad4c93
alexanderstracke <alexander.stracke@gmx.net>
2023-08-30 11:17:30 +0200

    updated backend classes: Controller.java, MongoDbRepository.java, Player.java, Service.java

--- backend/src/main/java/de/neuefische/backend/Service.java
+++ backend/src/main/java/de/neuefische/backend/Service.java
@@ -2,10 +2,48 @@ package de.neuefische.backend;
 
 import lombok.AllArgsConstructor;
 
+import java.util.List;
+
 @org.springframework.stereotype.Service
 @AllArgsConstructor
 
 public class Service {
 
+    private MongoDbRepository repository;
 
+    public void addPlayer(Player player) throws PlayerAlreadyExistException {
+
+        boolean containsPlayer = repository.existsById(player.getId());
+        if (containsPlayer) {
+            throw new PlayerAlreadyExistException("Player already exist");
+        } else {
+            repository.save(player);
+        }
+    }
+
+    public Player getPlayerById(String id) throws PlayerDoesNotExistException {
+        if (!repository.existsById(id)){
+            throw new PlayerDoesNotExistException("Player does not exist");
+        }
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Player> getAllPlayer() {
+        List<Player> listOfAllPlayer = repository.findAll();
+        return listOfAllPlayer;
+    }
+
+    public void deletePlayerById(String id) throws PlayerDoesNotExistException{
+        if(!repository.existsById(id)){
+            throw new PlayerDoesNotExistException("Player does not exist");
+        }
+        repository.deleteById(id);
+    }
+
+    public void updatePlayerById(String id, Player player) throws PlayerDoesNotExistException{
+        if(!repository.existsById(id)){
+            throw new PlayerDoesNotExistException("Player does not exist");
+        }
+        repository.save(player);
+    }
 }
